d3e395156edc06fba2effe3bc1d3692baa0a7468
Aaron France <aaron.l.france@gmail.com>
2014-10-21 20:13:00 +0200

    Add missing dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject aeronotix/crap "0.1.1"
+(defproject aeronotix/crap "0.2.2"
   :description "A bunch of crap"
   :url "https://github.com/AeroNotix/crap"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.logging "0.3.0"]])
