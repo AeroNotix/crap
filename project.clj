588c17ff27aefced2bb88886adf1c3a185211cd7
Aaron France <aaron.l.france@gmail.com>
2015-01-05 14:05:56 +0100

    Add with-ns

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject aeronotix/crap "0.4.7"
+(defproject aeronotix/crap "0.4.8"
   :description "A bunch of crap"
   :url "https://github.com/AeroNotix/crap"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [metrics-clojure "2.3.0"]
                  [org.clojure/tools.logging "0.3.0"]])
--- src/crap/control.clj
+++ src/crap/control.clj
@@ -11,6 +11,15 @@
 (defmacro retry [{:keys [retries wait-period]} & body]
   `(retry* ~retries ~wait-period (fn [] ~@body)))
 
+(defmacro with-ns
+  ;; Taken from clojure.contrib
+  "Evaluates body in another namespace.  ns is either a namespace
+  object or a symbol.  This makes it possible to define functions in
+  namespaces other than the current one."
+  [ns & body]
+  `(binding [*ns* (the-ns ~ns)]
+     ~@(map (fn [form] `(eval '~form)) body)))
+
 (defmacro with-temp-ns
   ;; Taken from clojure.contrib
   "Evaluates body in an anonymous namespace, which is then immediately
