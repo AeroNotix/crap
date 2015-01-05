c2d99a9017556183d9eaa1eb9c0cf7c612fcd77a
Aaron France <aaron.l.france@gmail.com>
2015-01-05 13:50:58 +0100

    Add with-temp-ns

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aeronotix/crap "0.4.5"
+(defproject aeronotix/crap "0.4.6"
   :description "A bunch of crap"
   :url "https://github.com/AeroNotix/crap"
   :license {:name "Eclipse Public License"
--- src/crap/control.clj
+++ src/crap/control.clj
@@ -10,3 +10,16 @@
 
 (defmacro retry [{:keys [retries wait-period]} & body]
   `(retry* ~retries ~wait-period (fn [] ~@body)))
+
+(defmacro with-temp-ns
+  ;; Taken from clojure.contrib
+  "Evaluates body in an anonymous namespace, which is then immediately
+  removed.  The temporary namespace will 'refer' clojure.core."
+  [& body]
+  `(try
+    (create-ns 'sym#)
+    (let [result# (with-ns 'sym#
+                    (clojure.core/refer-clojure)
+                    ~@body)]
+      result#)
+    (finally (remove-ns 'sym#))))
